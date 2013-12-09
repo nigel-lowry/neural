38c1a606ff84f96ac5195531b408019b38baefaf
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-09 17:47:22 +0000

    Add RSpec files

--- lib/neural.rb
+++ lib/neural.rb
@@ -2,4 +2,7 @@ require "neural/version"
 
 module Neural
   # Your code goes here...
+  class Perceptron
+
+  end
 end
---
+++ spec/perceptron_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+
+describe Perceptron do
+  it 'should do something' do
+    true.should == false
+  end
+end
