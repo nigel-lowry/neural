4141ca08b579b04d59e16c2657ae73fae834bd47
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-09 18:23:39 +0000

    Add sum method

--- lib/perceptron.rb
+++ lib/perceptron.rb
@@ -1,13 +1,19 @@
+require 'active_support/all'
+
 class Perceptron
 
   attr_reader :inputs
 
-  def initialize
-    @inputs = []
+  def initialize inputs
+    @inputs = inputs
   end
 
   def random_weight
     random_float_between_minus_one_and_one_inclusive
+  end
+
+  def sum
+    @inputs.collect {|input| input * random_weight }.sum
   end
 
 private
--- spec/perceptron_spec.rb
+++ spec/perceptron_spec.rb
@@ -3,6 +3,8 @@ require 'perceptron'
 
 describe Perceptron do
 
+  subject { Perceptron.new [12, 4] }
+
   it { should respond_to(:inputs) }
 
   describe "#random_weight" do
@@ -10,6 +12,13 @@ describe Perceptron do
       random_weight = subject.random_weight
       random_weight.should be >= -1.0
       random_weight.should be <= 1.0
+    end
+  end
+
+  describe "#sum" do
+    it "multiplies each input by its weight then adds them together" do
+      subject.stub(:random_weight).and_return(0.5, -1.0)
+      subject.sum.should == 2
     end
   end
 end
