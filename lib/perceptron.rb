dfd25708d41c67054139881fea3f9e0585a88633
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-09 18:10:13 +0000

    Add inputs array

--- lib/perceptron.rb
+++ lib/perceptron.rb
@@ -1,4 +1,11 @@
 class Perceptron
+
+  attr_reader :inputs
+
+  def initialize
+    @inputs = []
+  end
+
   def random_weight
     random_float_between_minus_one_and_one_inclusive
   end
--- spec/perceptron_spec.rb
+++ spec/perceptron_spec.rb
@@ -3,6 +3,8 @@ require 'perceptron'
 
 describe Perceptron do
 
+  it { should respond_to(:inputs) }
+
   describe "#random_weight" do
     it "should generate a random number between -1.0 and 1.0 inclusive" do
       random_weight = subject.random_weight
