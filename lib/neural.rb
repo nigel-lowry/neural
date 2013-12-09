547d58c32677eb4f76777474e7fff217980194e4
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-09 19:56:51 +0000

    Make start on driver app

--- lib/neural.rb
+++ lib/neural.rb
@@ -1,9 +1,34 @@
 require "neural/version"
+require 'perceptron'
+require 'trainer'
 
 module Neural
   class Driver
-    def say_hi
-      puts "hello"
+
+    @@xmin = -400
+    @@ymin = -100
+    @@xmax = 400
+    @@ymax = 100
+
+    def f x
+      0.4 * x + 1
+    end
+
+    def setup
+      @ptron = Perceptron.new 3
+      @training = []
+      @count = 0
+
+      2000.times do |n|
+        x = rand @@xmin..@@xmax
+        y = rand @@ymin..@@ymax
+        answer = 1
+        answer = -1 if y < f(x)
+        @training[n] = Trainer.new x, y, answer
+      end
+
+      @ptron.train @training[@count].inputs, @training[@count].answer
+      puts @ptron
     end
   end
 end
--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,4 +1,6 @@
 class Trainer
+  attr_reader :inputs, :answer
+
   def initialize x, y, a
     @inputs = [x, y, 1]
     @answer = a
