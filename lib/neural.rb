e0985f900732fcdcef6064a76ac7c39b501b5ea6
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-12-10 00:49:22 +0000

    Refactor

--- lib/neural.rb
+++ lib/neural.rb
@@ -4,30 +4,22 @@ require 'trainer'
 
 module Neural
   class Driver
-
-    def f x
-      0.4 * x + 1
-    end
-
     def setup
       ptron = Perceptron.new 3
 
-      2000.times do
+      2_000.times do
         x = rand -400..400
         y = rand -400..400
-
-        answer = if y < f(x)
-                   -1
-                 else
-                   1
-                 end
-
-        t = Trainer.new inputs: [x, y], answer: answer
-        ptron.train t.inputs, t.answer
+        trainer = Trainer.new inputs: [x, y], answer: y < f(x) ? -1 : 1
+        ptron.train trainer.inputs, trainer.answer
       end
 
       puts ptron.weights[0] / ptron.weights[1]
       puts ptron
+    end
+
+    def f x
+      0.4 * x + 1
     end
   end
 end
